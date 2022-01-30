46bbe6358b642f5de558cfb681b318f340e8e0f9
Cristian Milatinov <milatinovcristian@gmail.com>
2022-01-29 23:32:25 -0500

    Add vuex-persist

--- app/src/store.js
+++ app/src/store.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 
 import user from '@/modules/user.js';
 import cart from '@/modules/cart.js';
 import layout from '@/modules/layout.js';
 
 Vue.use(Vuex);
+
+const vuexLocal = new VuexPersistence({
+    storage: window.localStorage
+});
 
 export default new Vuex.Store({
     state: {},
@@ -15,5 +20,6 @@ export default new Vuex.Store({
         user,
         cart,
         layout
-    }
+    },
+    plugins: [vuexLocal.plugin]
 });
